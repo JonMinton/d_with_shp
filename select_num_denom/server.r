e723dfa650449681aa71cf1837bf8df14c6d0575
Jonathan Minton <nate.minton@gmail.com>
2015-03-30 17:51:38 +0100

    try to add isolate clauses

--- select_num_denom/server.r
+++ select_num_denom/server.r
@@ -31,22 +31,16 @@ sourceCpp("scripts/cppfunctions.cpp")
 
 la_to_dz <- read.csv("data/la_to_dz.csv") 
 
-las <- la_to_dz %>%
-  group_by(local_authority) %>%
-  summarise %>%
-  .$local_authority %>%
-  as.vector
-
-
- 
-
-      
-      
+las <- c("all",
+    la_to_dz %>%
+    group_by(local_authority) %>%
+    summarise %>%
+    .$local_authority %>%
+    as.vector
+  )
+     
       #####################################################################################
       #####################################################################################
-      
-
-
 
 shinyServer(function(input, output){
   
@@ -74,80 +68,82 @@ shinyServer(function(input, output){
   })
   
   generate_posterior_distribution <- reactive({
-    model <- run_model()
-    dta <- combine_input_table()
-    K <- input$posterior_sample_size      
-    go <- input$generate_posterior_button
-    
-    if (go){
-      out <- array(NA, K)
-      for(k in 1:K){
-        p.current <- exp(model$samples$phi[k ,] + model$samples$beta[k,1])   / (1 + exp(model$samples$phi[k ,] + model$samples$beta[k,1]))
-        p.current.overall <- sum(p.current *dta$denominator) / sum(dta$denominator)
-        out[k] <- sum(dta$denominator * abs(p.current - p.current.overall)) / ( 2 * sum(dta$denominator) * p.current.overall * (1-p.current.overall))                 
-      }
+    out <- NULL
+    observe({
+      go <- input$generate_posterior_button
+      model <- run_model()
+      dta <- combine_input_table()
+      K <- input$posterior_sample_size      
       
-      
-    } else {out <- NULL}
-    return(out)
+      if (go){
+        out <- array(NA, K)
+        for(k in 1:K){
+          p.current <- exp(model$samples$phi[k ,] + model$samples$beta[k,1])   / (1 + exp(model$samples$phi[k ,] + model$samples$beta[k,1]))
+          p.current.overall <- sum(p.current *dta$denominator) / sum(dta$denominator)
+          out[k] <- sum(dta$denominator * abs(p.current - p.current.overall)) / ( 2 * sum(dta$denominator) * p.current.overall * (1-p.current.overall))                 
+        }
+      }       
+    })
+    out
   })
   
   run_model <- reactive({
-    
-    w <- generate_w_matrix()
-    dta <- combine_input_table()
-    go <- input$run_model_button
-    
-    
-    if (go & !is.null(w) & !is.null(dta)){      
-      w_dz <- rownames(w)
-      # remove duplicates
-      w <- w[!duplicated(w_dz), !duplicated(w_dz)]
-      w_dz <- rownames(w)
-      dta_dz <- dta$datazone
-      ss <- intersect(w_dz, dta_dz)
-      tmp <- w_dz %in% ss
-      w <- w[tmp, tmp]
-      dta <- subset(dta, datazone %in% ss)
+    out <- NULL
+    observe({
+      input$run_model_button
       
-      #Adding try as workaround to issue that on some machines 
-      # S.CARiar works but on others iarCAR.re works, even 
-      # though the version of CARBayes reported is the same (4.0)
-      out <- try(S.CARiar(
-        formula= numerator  ~ 1,
-        trials = dta$denominator,
-        W=w,
-        data=dta,
-        family="binomial"
-      ))
-      if (class(out)=="try-error"){
-        out <- iarCAR.re(
+      w <- generate_w_matrix()
+      dta <- combine_input_table()
+      
+      if (!is.null(w) & !is.null(dta)){      
+        w_dz <- rownames(w)
+        # remove duplicates
+        w <- w[!duplicated(w_dz), !duplicated(w_dz)]
+        w_dz <- rownames(w)
+        dta_dz <- dta$datazone
+        ss <- intersect(w_dz, dta_dz)
+        tmp <- w_dz %in% ss
+        w <- w[tmp, tmp]
+        dta <- subset(dta, datazone %in% ss)
+        
+        #Adding try as workaround to issue that on some machines 
+        # S.CARiar works but on others iarCAR.re works, even 
+        # though the version of CARBayes reported is the same (4.0)
+        out <- try(S.CARiar(
           formula= numerator  ~ 1,
           trials = dta$denominator,
           W=w,
           data=dta,
-          family="binomial"          
-        )
+          family="binomial"
+        ))
+        if (class(out)=="try-error"){
+          out <- iarCAR.re(
+            formula= numerator  ~ 1,
+            trials = dta$denominator,
+            W=w,
+            data=dta,
+            family="binomial"          
+          )
+          
+        }
         
-      }
-      
-    } else {out <- NULL}
-    
-    return(out)
-    
+      } 
+    })
+    out
   })
   
   load_shapefiles <- reactive({
-    go <- input$load_shapefile_button
-    
-    if (go){
+    out <- NULL
+    observe({
+      input$load_shapefile_button
+       
       out <- readShapeSpatial(
         "shp/scotland_2001_datazones/scotland_dz_2001.shp"
       )      
       out@data <- rename(out@data, datazone=zonecode)
-      
-    } else {out <- NULL}
-    return(out)
+        
+      })
+    out
   })
   
   load_data <- reactive({
@@ -192,53 +188,53 @@ shinyServer(function(input, output){
   })
   
   generate_w_matrix <- reactive({
-    go <- input$make_w_matrix_button
-    if (go){
+    out <- NULL
+    observe({
+      input$make_w_matrix_button
       dta <- link_shp_with_attributes()
       if (!is.null(dta)){
         w_nb <- poly2nb(dta)
         out <- nb2mat(w_nb, style="B", zero.policy=TRUE)
         rownames(out) <- colnames(out) <- dta@data$datazone        
-      } else { out <- NULL}      
-    } else {out <- NULL}
-    return(out)
+        }  
+      })      
+    })
+    out
   })
   
   combine_input_table <- reactive({
-
-    
-    go <- input$ok_num_denom
-    
-    numerators <- isolate(input$numerator_selection)
-    denominators <- isolate(input$denominator_selection)
-    
-    if (go & !is.null(numerators) & !is.null(denominators)){
-      cat("numerators: ")
-      for (i in 1:length(numerators)) {cat(numerators[i], "\n")}
-      cat("\n\n")
-
-      cat("denominators: ")
-      for (i in 1:length(denominators)) {cat(denominators[i], "\n")}
-      cat("\n\n")
-      
-      
-      data_raw <- load_data()
-      data_raw <- data_raw %>% dplyr::select(datazone, type, count)
-      
-      data_numerator <- data_raw %>% 
-        filter(type %in% numerators) %>%
-        group_by(datazone) %>% summarise(numerator=sum(count))
-      
-      data_denominator <- data_raw %>% 
-        filter(type %in% denominators) %>%
-        group_by(datazone) %>% summarise(denominator=sum(count))
-      
-      
-      data_out <- inner_join(data_denominator, data_numerator)
-      out <- data_out                  
-    } else {out <- NULL}
-
-    return(out)
+    out <- NULL
+    observe({
+      input$ok_num_denom
+      numerators <- isolate(input$numerator_selection)
+      denominators <- isolate(input$denominator_selection)
+      if (!is.null(numerators) & !is.null(denominators)){
+        cat("numerators: ")
+        for (i in 1:length(numerators)) {cat(numerators[i], "\n")}
+        cat("\n\n")
+        
+        cat("denominators: ")
+        for (i in 1:length(denominators)) {cat(denominators[i], "\n")}
+        cat("\n\n")
+        
+        
+        data_raw <- load_data()
+        data_raw <- data_raw %>% dplyr::select(datazone, type, count)
+        
+        data_numerator <- data_raw %>% 
+          filter(type %in% numerators) %>%
+          group_by(datazone) %>% summarise(numerator=sum(count))
+        
+        data_denominator <- data_raw %>% 
+          filter(type %in% denominators) %>%
+          group_by(datazone) %>% summarise(denominator=sum(count))
+        
+        
+        data_out <- inner_join(data_denominator, data_numerator)
+        out <- data_out                  
+      } 
+    })
+    out
   })
   
   calc_d_classical <- reactive({
@@ -346,6 +342,5 @@ shinyServer(function(input, output){
     }
     return(out)
   }) 
-  
-  
-})
+
+}
